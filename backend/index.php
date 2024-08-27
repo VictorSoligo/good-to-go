a450485f76409dda9c851fb1bcf9bb9d623bee41
Victor Hugo Soligo <victorsoligo@gmail.com>
2024-08-27 20:39:36 -0300

    chore: database connection setup

--- backend/index.php
+++ backend/index.php
@@ -1,15 +1,24 @@
 <?php
 
+use App\Database\Connection;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';    
+require __DIR__ . '/src/Env/index.php';
 
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response) {
-    $data = ['ok', true];
+    $instance = Connection::getInstance();
+    $mysql = $instance->getConnection();
+
+    $stmt = $mysql->query("SELECT * FROM users");
+    $users = $stmt->fetchAll();
+
+    $data = ['users', $users];
+
     $payload = json_encode($data);
 
     $response->getBody()->write($payload);
